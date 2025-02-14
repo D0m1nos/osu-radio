8d7ec0264cb23248b3646f33d78c65f273255ca4
D0m1nos <21263344+D0m1nos@users.noreply.github.com>
2025-02-14 23:19:35 +0100

    Fisher-Yates shuffle

--- src/shared/lib/tungsten/collections.ts
+++ src/shared/lib/tungsten/collections.ts
@@ -5,8 +5,8 @@ import { flatRNG } from "./math";
  * @param array
  */
 export function shuffle<T>(array: T[]): void {
-  for (let i = 0; i < array.length; i++) {
-    const j = flatRNG(0, array.length);
+  for (let i = 0; i < array.length - 1; ++i) {
+    const j = flatRNG(i, array.length);
 
     const t = array[i];
     array[i] = array[j];
